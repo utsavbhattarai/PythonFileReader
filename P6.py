b73db9aa8bcbe34f31d8de3c7cf422d8cf8070ae
Utsav Bhattarai <utsav.bhattaraicode@gmail.com>
2019-11-05 21:39:28 -0600

    minor changes

--- P6.py
+++ P6.py
@@ -5,12 +5,13 @@ file_name = sys.argv[1]
 with ZipFile(file_name, 'r') as zip: 
     AllFiles = zip.namelist()
     zip.extractall()
-    print(AllFiles)   
+    #print(AllFiles)   
     files = [open(name) for name in AllFiles]
     sets = [set(line.strip() for line in file) 
             for file in files]
     common = set.intersection(*sets)
-    print(common)
+    #print(common)
+print("Please check the output file for the common")    
 with open('output.txt', 'w') as file_out:
     for line in common:        
         print(line)
